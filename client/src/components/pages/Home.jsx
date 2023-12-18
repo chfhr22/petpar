9fe0632cc55b392625c5e24097544ed721bd6a1b
rlanrid <rladnrider77@gmail.com>
2023-12-18 15:11:18 +0900

    home페이지 수정

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Filter from '../homes/Filter.jsx'
+import Contents from '../homes/Contents.jsx'
 
 const Home = () => {
     return (
         <div id='homePage' className='pages'>
-            <div className='home__wrap'>
-
-            </div>
+            <Filter />
+            <Contents />
         </div>
     )
 }
