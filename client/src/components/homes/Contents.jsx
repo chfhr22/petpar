6466eea2afefe2ab041d4cbfdb80d0a4716825ed
rlanrid <rladnrider77@gmail.com>
2023-12-19 15:02:40 +0900

    오류수정

--- client/src/components/homes/Contents.jsx
+++ client/src/components/homes/Contents.jsx
@@ -42,71 +42,72 @@ const Contents = () => {
                 console.log(err);
             }
         }
+    }
 
-        useEffect(() => {
-            fetchInfo('all');
-        }, [])
+    useEffect(() => {
+        fetchInfo('all');
+    }, [])
 
-        return (
-            <>
-                <div className='filter__wrap'>
-                    <ul>
-                        <li><Link to="/" onClick={() => fetchInfo('all')} className={activeCategory === 'all' ? 'active' : ''}>전체</Link></li>
-                        <li><Link to="/">팔로우</Link></li>
-                        <li><Link to="/" onClick={() => fetchInfo('dog')} className={activeCategory === 'dog' ? 'active' : ''}>강아지</Link></li>
-                        <li><Link to="/" onClick={() => fetchInfo('cat')} className={activeCategory === 'cat' ? 'active' : ''}>고양이</Link></li>
-                    </ul>
-                </div>
+    return (
+        <>
+            <div className='filter__wrap'>
+                <ul>
+                    <li><Link to="/" onClick={() => fetchInfo('all')} className={activeCategory === 'all' ? 'active' : ''}>전체</Link></li>
+                    <li><Link to="/">팔로우</Link></li>
+                    <li><Link to="/" onClick={() => fetchInfo('dog')} className={activeCategory === 'dog' ? 'active' : ''}>강아지</Link></li>
+                    <li><Link to="/" onClick={() => fetchInfo('cat')} className={activeCategory === 'cat' ? 'active' : ''}>고양이</Link></li>
+                </ul>
+            </div>
 
-                <div className='contents__wrap'>
-                    {petItems.map((item, key) => (
-                        <div className="contents" key={key}>
-                            <div className="contents__top">
-                                <div className="shelter">{item.careNm}</div>
-                                <div className="total__like">
-                                    <img src={heart} alt="하트이미지" />
-                                    <p>945</p>
+            <div className='contents__wrap'>
+                {petItems.map((item, key) => (
+                    <div className="contents" key={key}>
+                        <div className="contents__top">
+                            <div className="shelter">{item.careNm}</div>
+                            <div className="total__like">
+                                <img src={heart} alt="하트이미지" />
+                                <p>945</p>
+                            </div>
+                            <div className="image">
+                                <img src={item.popfile} alt="게시글이미지" />
+                            </div>
+                        </div>
+                        <div className="contents__bottom">
+                            <div className="info">
+                                <div className="left">
+                                    <div className="profile">
+                                        <img src={profile} alt="프로필이미지" />
+                                    </div>
+                                    <div className="name">{item.chargeNm}</div>
+                                    <div className="date">3일</div>
+                                    <div className="call"><IoCallOutline /></div>
                                 </div>
-                                <div className="image">
-                                    <img src={item.popfile} alt="게시글이미지" />
+                                <div className="right">
+                                    <div className="like"><IoHeartOutline /></div>
+                                    <div className="share"><IoShareSocialSharp /></div>
+                                    <div className="bookmark"><IoBookmarkOutline /></div>
                                 </div>
                             </div>
-                            <div className="contents__bottom">
-                                <div className="info">
-                                    <div className="left">
-                                        <div className="profile">
-                                            <img src={profile} alt="프로필이미지" />
-                                        </div>
-                                        <div className="name">{item.chargeNm}</div>
-                                        <div className="date">3일</div>
-                                        <div className="call"><IoCallOutline /></div>
-                                    </div>
-                                    <div className="right">
-                                        <div className="like"><IoHeartOutline /></div>
-                                        <div className="share"><IoShareSocialSharp /></div>
-                                        <div className="bookmark"><IoBookmarkOutline /></div>
-                                    </div>
+                            <div className="board">
+                                <div className="title">{item.specialMark}</div>
+                                <div className={expandedItems[key] ? `all` : `content`}>
+                                    품종 : {item.kindCd}<br />
+                                    나이 : {item.age}<br />
+                                    색깔 : {item.colorCd}<br />
+                                    몸무게 : {item.weight}<br />
+                                    상태 : {item.processState}<br />
+                                    특징 : {item.specialMark}
                                 </div>
-                                <div className="board">
-                                    <div className="title">{item.specialMark}</div>
-                                    <div className={expandedItems[key] ? `all` : `content`}>
-                                        품종 : {item.kindCd}<br />
-                                        나이 : {item.age}<br />
-                                        색깔 : {item.colorCd}<br />
-                                        몸무게 : {item.weight}<br />
-                                        상태 : {item.processState}<br />
-                                        특징 : {item.specialMark}
-                                    </div>
-                                    <div className='more' onClick={() => toggleExpand(key)}>
-                                        {expandedItems[key] ? '접기' : '더보기'}
-                                    </div>
+                                <div className='more' onClick={() => toggleExpand(key)}>
+                                    {expandedItems[key] ? '접기' : '더보기'}
                                 </div>
                             </div>
                         </div>
-                    ))}
-                </div>
-            </>
-        )
-    }
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
 
-    export default Contents
+export default Contents
