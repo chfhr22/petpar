6fcb364f2ea4f0d95c0fe8c06d263c6851a522cd
rlanrid <rladnrider77@gmail.com>
2023-12-26 14:46:40 +0900

    푸터수정

--- client/src/components/section/Footer.jsx
+++ client/src/components/section/Footer.jsx
@@ -1,26 +1,39 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Footer = () => {
+    const location = useLocation();
+
     return (
         <div id='footerSection' className='sec'>
-            <h2>실시간 채팅</h2>
-            <div className='chating__wrap'>
-                <div className='chating'>
+            {location.pathname === "/find" ? (
+                <>
 
-                    <div className="chating__box">
-                        <div className='chating__author'><span className='name'>초록</span> <span className='time'>14:58</span></div>
-                        <div className='chating__cont'>
-                            ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+                </>
+            ) : (
+                <>
+                    <h2>실시간 채팅</h2>
+                    <div className='chating__wrap'>
+                        <div className='chating'>
+
+                            <div className="chating__box">
+                                <div className='chating__author'><span className='name'>초록</span> <span className='time'>14:58</span></div>
+                                <div className='chating__cont'>
+                                    ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+                                </div>
+                            </div>
+
                         </div>
-                    </div>
 
-                </div>
-                
-            </div>
-            <div className='chating__submit'>
-                    <label htmlFor="live__chat" className='blind' />
-                    <input type="text" id='live__chat' name='live__chat' />
-                </div>
+                    </div>
+                    <div className='chating__submit'>
+                        <label htmlFor="live__chat" className='blind' />
+                        <input type="text" id='live__chat' name='live__chat' />
+                    </div>
+                </>
+            )}
+
+
         </div>
     )
 }
