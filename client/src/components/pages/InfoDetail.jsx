d66d6d2bcbbfdca790b59703e52bbc32b2ee55df
wonyoung <elese0821@naver.com>
2023-12-27 18:08:03 +0900

    ddddd

--- client/src/components/pages/InfoDetail.jsx
+++ client/src/components/pages/InfoDetail.jsx
@@ -7,22 +7,21 @@ const InfoDetail = () => {
   const { infoId } = useParams();
   const infoNum = Info[infoId]
 
-  console.log("infoId : " ,infoId)
+  console.log("infoId : ", infoId)
   return (
     <div id='InfoDetailPage' className='pages'>
-        <div className='info__detail__page'>
-          <h2 className='info__detail__title'>
-            {infoNum.title}
-          </h2>
-          <div className="info__detail__author">
-            <div className="author">{infoNum.author}</div>
-            <div className="date">{infoNum.date}</div>
-          </div>
-          <div className="info__detail__cont">
-            <div className="info__detail__img"><img src={`${infoNum.img}`} alt="" /></div>
-            <div className="info__detail__text">
-              {infoNum.desc}
-            </div>
+      <div className='info__detail__page'>
+        <h2 className='info__detail__title'>
+          {infoNum.title}
+        </h2>
+        <div className="info__detail__author">
+          <div className="author">{infoNum.author}</div>
+          <div className="date">{infoNum.date}</div>
+        </div>
+        <div className="info__detail__cont">
+          <div className="info__detail__img"><img src={`${infoNum.img}`} alt="" /></div>
+          <div className="info__detail__text">
+            {infoNum.desc}
           </div>
         </div>
 
@@ -31,6 +30,7 @@ const InfoDetail = () => {
           <Link to="/info" className='lsit'>목록으로</Link>
           <Link to="/" className='next'>다음으로</Link>
         </div>
+      </div>
     </div>
   )
 }
